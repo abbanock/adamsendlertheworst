5ac272ab7ecb08bcd0816bbf4cf328df1585d04f
Zor Abbanock <emirate1909@gmail.com>
2024-06-13 12:30:37 +0400

    kitty

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+//import JSConfetti from 'js-confetti'
+
+
+const container = document.querySelector('.container');
 const yesBtn = document.querySelector('.yes');
 const noBtn = document.querySelector('.no');
 const kittyGif = document.querySelector('.neutral');
@@ -6,29 +10,43 @@ const angryKitty = document.querySelector('.angry');
 let noWidth = noBtn.style.width;
 let noHeight = noBtn.style.height;
 
+
+
 noBtn.addEventListener('click', event => {
-            kittyGif.style.display = "none";
-            angryKitty.style.display = "inline";
+    kittyGif.style.display = "none";
+    angryKitty.style.display = "inline";
 
-            let width = noBtn.offsetWidth;
-            let height = noBtn.offsetHeight;
-            let fontSize = parseInt(window.getComputedStyle(noBtn).fontSize);
+    let width = noBtn.offsetWidth;
+    let height = noBtn.offsetHeight;
+    let fontSize = parseInt(window.getComputedStyle(noBtn).fontSize);
 
-            width *= 0.9;
-            height *= 0.9;
-            fontSize *= 0.9;
+    width *= 0.9;
+    height *= 0.9;
+    fontSize *= 0.9;
 
-            noBtn.style.width = width + "px";
-            noBtn.style.height = height + "px";
-            noBtn.style.fontSize = fontSize + "px";
+    noBtn.style.width = width + "px";
+    noBtn.style.height = height + "px";
+    noBtn.style.fontSize = fontSize + "px";
 
-            if (noBtn.offsetWidth < 80) {
-                noBtn.style.display = "none";
-            }
-            })
+    if (noBtn.offsetWidth < 80) {
+        noBtn.style.display = "none";
+    }
+})
 
 yesBtn.addEventListener('click', event => {
-                    kittyGif.style.display = "none";
-                    angryKitty.style.display = "none";
-                    happyKitty.style.display = "inline";
-                })
+    const jsConfetti = new JSConfetti({
+        container
+    })
+    jsConfetti.addConfetti({
+        confettiColors: [
+    '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7', 'red', 'blue', 'green', 'yellow', 'orange'
+  ],
+        confettiRadius: 5,
+        confettiNumber: 500,
+    })
+
+    kittyGif.style.display = "none";
+    angryKitty.style.display = "none";
+    happyKitty.style.display = "inline";
+
+})
